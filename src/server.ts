3ab13ac5bd71b711de991aecc7d7dc0d51c8ca1a
João Alves <alvezsjv@gmail.com>
2022-06-16 21:55:08 -0300

    feat(server): add cookie parser

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,27 @@
 import express from 'express'
 import { routes } from './routes'
 import cors from 'cors'
+import { allowedOrigins } from './config/AllowedOrigins';
+import { credentials } from './middlewares/Credentials';
+import cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(cors());
+app.use(credentials);
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (allowedOrigins.indexOf(origin || '') !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+}));
+
 app.use(express.json());
+app.use(cookieParser());
 app.use(routes);
 
 app.listen(3333, () => {
